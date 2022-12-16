5d9cf639ef4dbc5e40db8bfe2a2bd4d571e51ea3
ranggawahyuds <ranggawahyuds@gmail.com>
2022-12-16 15:58:50 +0700

    .

--- application/views/order/ubah_order.php
+++ application/views/order/ubah_order.php
@@ -31,6 +31,7 @@
                     </div>
                     <div class="form-group">
                         <select class="form-control form-control-user" name="status_order" id="status_order">
+                            <option value="">--->Status Order<---</option>
                             <option value="selesai">Selesai</option>
                             <option value="proses">Proses</option>
                         </select>
